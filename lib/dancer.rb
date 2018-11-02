2cfd6380c27cfb9e8f73ea1184fe623eca3f560f
SANJAY K SAI <shachsan@hotmail.com>
2018-11-02 17:34:42 +0000

    automatically backed up by learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,7 +1,7 @@
 require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
 class Dancer
-  include Fancjy
+  include FancyDance::
   extend MetaDancing
 
   attr_accessor :name
