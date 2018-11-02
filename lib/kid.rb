68a7dfe7df408aa814bd20bbbd7780ddd2b9421a
SANJAY K SAI <shachsan@hotmail.com>
2018-11-02 17:34:23 +0000

    automatically backed up by learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -2,6 +2,7 @@ require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
 class Kid
   include FancyDance::InstanceMethods
+  include FancyDance
 
   attr_accessor :name
   def initialize(name)
